3389c717b95eaf6e97e6bb36a00a08d438689f63
Ameer-Alaswad <ammeer.z.alaswad@gmail.com>
2023-12-21 01:40:03 +0100

    adds services-overview and style it

---
+++ src/components/services-overview/styles.tsx
@@ -0,0 +1,38 @@
+export const servicesImagesListMainContainerStyles = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+}
+export const servicesImagesListContainer = {
+    height: 900,
+    "@media (max-width: 866px)": {
+        height: "100%",
+        display: "contents",
+        width: "100%"
+    },
+};
+export const serviceImageItemContainerStyles = {
+    backgroundColor: "#F6F6F6",
+    textAlign: "center",
+    height: "120px"
+}
+export const servicesTitleStyles = {
+    textAlign: "center",
+    fontWeight: "bold",
+    fontFamily: "'monospace','Arial', sans-serif",
+    marginBottom: "20px",
+    fontSize: "50px",
+    marginTop: "20px",
+
+    "@media (max-width: 866px)": {
+        fontSize: "30px",
+
+    },
+}
+export const serviceImageTitleStyles = {
+    fontWeight: "bold",
+    marginTop: "5px",
+    marginBottom: "5px"
+}
+export const serviceImageDescriptionStyles = { width: "90%", whiteSpace: "normal", margin: "0 auto" }
